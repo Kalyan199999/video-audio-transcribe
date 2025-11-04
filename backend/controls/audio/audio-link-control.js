331a321ead82ccf16978930cc9deffbb8ace43a5
kalyan199999 <gottapupavankalyan@gmail.com>
2025-11-04 12:45:17 +0530

    audio link control in AI-transcribe is added!

---
+++ backend/controls/audio/audio-link-control.js
@@ -0,0 +1,70 @@
+const Audio = require('../../model/audio_model')
+const { genAIAudioLinkTranscribe } = require('../../gemini-api-models/gen-ai-audio-transcribe')
+
+const get_link_audio = async (req,res) => {
+
+    try 
+    {
+        const audio = await Audio.find().sort({createdAt:-1})
+
+        return res.status(200).json({
+            ok:true,
+            data:audio,
+            message:"Audios fetched successfully"
+        })
+    } 
+    catch (error) 
+    {
+        return res.status(500).json({
+            ok:false,
+            data:null,
+            message:"Error while fetching audio"
+        })
+    }
+}
+
+const post_link_audio = async (req,res) => {
+    
+    try 
+    {
+        const { url } = req.body
+
+        const audio = await genAIAudioLinkTranscribe(url)
+
+        if( !audio.ok  )
+        {
+            return res.status(500).json({
+                ok:false,
+                data:null,
+                message:"Error while transcribing audio!",
+                audio
+            })
+        }
+
+        const data = new Audio({
+            url:url,
+            text:audio.data
+        })
+
+        await data.save()
+
+        return res.status(200).json({
+            ok:true,
+            data:data,
+            message:"Audio added successfully"
+        })
+    } 
+    catch (error) 
+    {
+        return res.status(500).json({
+            ok:false,
+            data:null,
+            message:"Error while adding audio!"
+        })
+    }
+}
+
+module.exports = {
+    get_link_audio,
+    post_link_audio
+}
--- backend/routes/audio/audio-link-route.js
+++ backend/routes/audio/audio-link-route.js
@@ -1,17 +1,22 @@
 const express = require('express');
+const {
+    get_link_audio,
+    post_link_audio
+} = require('../../controls/audio/audio-link-control')
+
 const verify = require('../../middleware/verifyToken')
 const router = express.Router();
 
-router.get('/' ,   ()=>{
-    
-});
+router.get('/' ,   get_link_audio );
 
 router.get('/:id', ()=>{
+
+    return resizeBy.status(200).json({
+        message: 'get link audio'
+    })
     
 } );
 
-router.post('/',verify, ()=>{
-    
-} );
+router.post('/',verify,post_link_audio);
 
 module.exports = router;
